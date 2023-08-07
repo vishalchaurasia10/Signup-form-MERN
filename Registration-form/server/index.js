cf2991a66d6fbb392513daf8594c70bfb5b48bd5
Mridul Tripathi <98095736+mridul588@users.noreply.github.com>
2023-08-07 18:48:58 +0530

    Update index.js

--- Registration-form/server/index.js
+++ Registration-form/server/index.js
@@ -13,10 +13,18 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
+app.use(cors(
+    {
+        origin: [""].
+        methods: ["POST" , "GET"],
+        credentials: true
+    }
+    ));
 
 app.use("/",require("./routes/authRoute"));
 
-const port = 8000;
+// const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port , () => {
     console.log(`server running on port ${port}`);
